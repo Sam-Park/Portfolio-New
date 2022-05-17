224dc3fb8647782010bbcc9b491f4cf90f5e3cbc
Sam Park <samuel.culver.park@gmail.com>
2022-05-16 20:51:46 -0400

    mvp

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:774-269-6906'>774-269-6906</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:sam@samuelcpark.com'>sam@samuelcpark.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+      <SocialContainer>
+        <SocialIcons href="https://github.com/Sam-Park">
+        <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/sam-park-b6266b66/">
+        <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+      </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
